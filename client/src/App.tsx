30bd410e955a30433e568c7e6f31d228678670e5
Mog Rouhi <moghadase.roohi@gmail.com>
2022-12-17 22:00:08 +0000

    fix cors issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [products, setProducts] = useState([
+    {name: "product1", price: 100.00},
+    {name: "product2", price: 200.00},
+  ]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+    .then(response => response.json())
+    .then(data => setProducts(data))
+  }, [])
+
+
   return (
     <div>
           <h1>Re-Store</h1>
+          <ul>
+          {products.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+          </ul>
     </div>
   );
 }
