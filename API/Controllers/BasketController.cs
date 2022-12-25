4303ff0f1d63bdae917f22fd04601da9d48c0313
Mog Rouhi <moghadase.roohi@gmail.com>
2022-12-25 11:27:10 +0000

    add api endpoints in basket controller

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,7 +1,8 @@
-using System.Linq;
+using System;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,7 @@ namespace API.Controllers
     public class BasketController : BaseApiController
     {
         private readonly StoreContext _context;
+
         public BasketController(StoreContext context)
         {
             _context = context;
@@ -18,12 +20,10 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<Basket>> GetBasket()
         {
-            var basket = await _context.Baskets
-                .Include(i => i.Items)
-                .ThenInclude(p => p.Product)
-                .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+            var basket = await RetrieveBasket();
 
-            if (basket == null) return NotFound();
+            if (basket == null)
+                return NotFound();
 
             return basket;
         }
@@ -31,9 +31,55 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
         {
-            // get basket
+            var basket = await RetrieveBasket();
 
-            return StatusCode(202);
+            if (basket == null)
+                basket = CreateBasket();
+            
+            var product = await _context.Products.FindAsync(productId);
+            
+            if (product == null)
+                return NotFound();
+            
+            basket.AddItem(product, quantity);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result)
+                return StatusCode(201);
+
+            return BadRequest(new ProblemDetails { Title = "Problem saving item to basket" });
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
+        {
+            // get basket
+            // remove item or reduce quantity
+            // save changes
+            return Ok();
+        }
+
+        private async Task<Basket> RetrieveBasket()
+        {
+            return await _context.Baskets
+                .Include(i => i.Items)
+                .ThenInclude(p => p.Product)
+                .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+        }
+
+        private Basket CreateBasket()
+        {
+            var buyerId = Guid.NewGuid().ToString();
+            var cookieOptions = new CookieOptions
+            {
+                IsEssential = true,
+                Expires = DateTime.Now.AddDays(30)
+            };
+            Response.Cookies.Append("buyerId", buyerId, cookieOptions);
+            var basket = new Basket { BuyerId = buyerId };
+            _context.Baskets.Add(basket);
+            return basket;
         }
     }
 }
