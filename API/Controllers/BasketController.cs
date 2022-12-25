478422a827572fb5a53ea5131159e368aad78a84
Mog Rouhi <moghadase.roohi@gmail.com>
2022-12-25 10:41:09 +0000

    create basket entities, migrations

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BasketController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await _context.Baskets
+                .Include(i => i.Items)
+                .ThenInclude(p => p.Product)
+                .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+
+            if (basket == null) return NotFound();
+
+            return basket;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+        {
+            // get basket
+
+            return StatusCode(202);
+        }
+    }
+}
--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -8,5 +8,6 @@ namespace API.Data
         public StoreContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
     }
 }
