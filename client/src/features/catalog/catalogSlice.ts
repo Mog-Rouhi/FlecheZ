58af328f8579d36650afd22b97762951b1e7f396
Mog Rouhi <moghadase.roohi@gmail.com>
2022-12-26 11:59:08 +0000

    handle loading state

---
+++ client/src/features/catalog/catalogSlice.ts
@@ -0,0 +1,47 @@
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from "@reduxjs/toolkit";
+import agent from "../../app/api/agent";
+import { Product } from "../../app/models/product";
+import { RootState } from "../../app/store/configureStore";
+
+const productsAdapter = createEntityAdapter<Product>();
+
+export const fetchProductsAsync = createAsyncThunk<Product[]>(
+  "catalog/fetchProductsAsync",
+  async () => {
+    try {
+      return await agent.Catalog.list();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const catalogSlice = createSlice({
+  name: "catalog",
+  initialState: productsAdapter.getInitialState({
+    productsLoaded: false,
+    status: "idle",
+  }),
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProductsAsync.pending, (state) => {
+      state.status = "pendingFetchProducts";
+    });
+    builder.addCase(fetchProductsAsync.fulfilled, (state, action) => {
+      productsAdapter.setAll(state, action.payload);
+      state.status = "idle";
+      state.productsLoaded = true;
+    });
+    builder.addCase(fetchProductsAsync.rejected, (state) => {
+      state.status = "idle";
+    });
+  },
+});
+
+export const productSelectors = productsAdapter.getSelectors(
+  (state: RootState) => state.catalog
+);
