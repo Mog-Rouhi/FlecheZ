05dd0c437b8c77f84c003f826c7d85d82799dea0
Mog Rouhi <moghadase.roohi@gmail.com>
2022-12-30 12:38:26 +0000

    add ASP.NET identity and ef migration for user

---
+++ API/Entities/User.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Entities
+{
+    public class User : IdentityUser
+    {
+        
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,9 @@
 using API.Data;
+using API.Entities;
 using API.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +34,12 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddCors();
+            services
+                .AddIdentityCore<User>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<StoreContext>();
+            services.AddAuthentication();
+            services.AddAuthorization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
